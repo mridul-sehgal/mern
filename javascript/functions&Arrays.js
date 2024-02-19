e80f0c67c708cfd20e3db4632917c7f7f7c0026f
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-02-19 13:33:54 +0530

    async javascript :- setTimeout, promises,await ;  objects

--- javascript/functions&Arrays.js
+++ javascript/functions&Arrays.js
@@ -1,29 +1,23 @@
-
 // ********** Functions **********
 
-function hello ()
-{
-    console.log("Hello function ");
+function hello() {
+  console.log("Hello function ");
 }
-hello()
+hello();
 
-
-function parameter(a,b) {
-    console.log(a+b);    
+function parameter(a, b) {
+  console.log(a + b);
 }
 
-parameter(2,3)
+parameter(2, 3);
 
-// Arrow function:- if your function has only single line of code then
-//the arrow function can be used. They are functions with no name and has 
-//only single line of code
+// Arrow function:-  anonymous functions i.e. functions without a name but they are
+// often assigned to any variable.
 
-let sum =(a,b) => a+b;
-console.log(sum(4,3));
-
+let sum = (a, b) => a + b;
+console.log(sum(4, 3));
 
 // ********** Arrays **********
 
-let arr=[1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5];
 console.log(arr);
-
