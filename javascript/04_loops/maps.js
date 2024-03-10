c2addd1bae420d9c0e94e3b92d654167dcee0389
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-03-11 01:40:00 +0530

    map and filter

---
+++ javascript/04_loops/maps.js
@@ -0,0 +1,4 @@
+// MAPS ARE SIMILAR TO FILTERS IN JAVASCRIPT. IT ALSO HAS A CALL BACK FUNCTION IN IT.
+const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNumbers.map((num)=> num+10);
+console.log(newNums)
